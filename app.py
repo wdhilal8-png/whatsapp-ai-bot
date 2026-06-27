1ca5caf56d4158ab4b0be7343cfad07ee70dcbae
wdhilal8-png <wdhilal8@gmail.com>
2026-06-27 19:09:52 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,28 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
+VERIFY_TOKEN = "almoez123"
+
 @app.route("/")
 def home():
-    return "WhatsApp AI Bot is running!"
+    return "WhatsApp Bot is Running!"
+
+@app.route("/webhook", methods=["GET", "POST"])
+def webhook():
+    if request.method == "GET":
+        token = request.args.get("hub.verify_token")
+        challenge = request.args.get("hub.challenge")
+
+        if token == VERIFY_TOKEN:
+            return challenge
+
+        return "Verification failed", 403
+
+    if request.method == "POST":
+        data = request.get_json()
+        print(data)
+        return "EVENT_RECEIVED", 200
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)
